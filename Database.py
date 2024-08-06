4ae3a10d76e673727a5ec96c7ea1e191b611267b
Rahul Verma <148262333+Rahulverma-09@users.noreply.github.com>
2024-08-06 12:11:01 +0000

    Connected with cloud database server and fetch n display information from the database

---
+++ Database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine, text
+import os
+
+engine = create_engine(os.environ['DB_CONNECTION'])
+
+# Function to get all the items info from the database
+def load_items_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text('select * from handicraft'))
+    
+        items_info = []
+        for row in result.fetchall():
+            new_dict = {"Item_id": row.Item_id, "Item_name": row.Item_name, "Metal_name": row.Metal_name,
+                        "Weight":row.Weight, "Polish":row.Polish, "Price":row.Price, "Image": "img.jpg"}
+            items_info.append(new_dict)
+        return items_info
--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
+from Database import load_items_from_db
 
 app = Flask(__name__)
+
 @app.route("/")
-def hello():
-    return render_template("home.html")
+def webservice():
+    item_info = load_items_from_db()
+    return render_template("home.html", info=item_info)
   
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8080)
+
+
