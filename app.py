988c627fa7f2c40b879ed96584fed5146b1b7943
Rahul Verma <148262333+Rahulverma-09@users.noreply.github.com>
2024-08-12 12:47:44 +0000

    Added more templates and routes

--- app.py
+++ app.py
@@ -4,10 +4,22 @@ from Database import load_items_from_db
 app = Flask(__name__)
 
 @app.route("/")
-def webservice():
+def display_home():
+    return render_template("home.html")
+    
+@app.route("/items")
+def display_items():
     item_info = load_items_from_db()
-    return render_template("home.html", info=item_info)
-  
+    return render_template("items.html", info=item_info)
+
+@app.route("/contact")
+def display_contacts():
+    return render_template("contact.html")
+
+@app.route("/about")
+def display_about():
+    return render_template("about.html")
+    
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8080)
 
